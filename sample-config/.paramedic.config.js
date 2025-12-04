de7e6f0df9a9de3b464014cc086cc48224ec3d1b
エリス <erisu@users.noreply.github.com>
2025-12-05 02:26:16 +0900

    feat!: remove deprecated windows platform (#277)

--- sample-config/.paramedic.config.js
+++ sample-config/.paramedic.config.js
@@ -24,7 +24,7 @@ module.exports = {
     "plugins": [
         "https://github.com/apache/cordova-plugin-inappbrowser"
     ],
-     "platform": "windows@latest",
+     "platform": "android",
      "action": "run",
-     "args": "--archs=x64 -- --appx=uap"
+     "args": ""
 };
